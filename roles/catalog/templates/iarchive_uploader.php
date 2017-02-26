20486b70aec28a84f8db589cb1bf851c0eaf71c2
Artom Lifshitz <notartom@gmail.com>
2017-02-26 16:17:17 -0500

    Deploy iarchive_uploader.php with ansible

---
+++ roles/catalog/templates/iarchive_uploader.php
@@ -0,0 +1,13 @@
+<?php                                                                                           
+
+// these can be reset at: http://archive.org/account/s3.php
+
+//make this config ['iarchive_uploader'] so we can just grab $config['ias3uploader'] to pass to library
+$config['iarchive_uploader']['access_key'] = '{{ iarchive_uploader_access_key }}';
+$config['iarchive_uploader']['secret_key'] = '{{ iarchive_uploader_secret_key }}';
+
+//if there is a direct iarchive alias, let's use that in case the s3 link changes
+$config['iarchive_uploader']['amazon_endpoint'] = 'http://s3.us.archive.org';
+
+// iarchive project page
+$config['iarchive_uploader']['iarchive_project_page'] = 'http://archive.org/details';
