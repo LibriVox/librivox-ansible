2c07d9f8773efd9bd4de39573c0b69fa03a374bf
Artom Lifshitz <notartom@gmail.com>
2018-10-20 18:47:38 -0400

    Update config files for CodeIgniter 3
    Update config.php and database.php for CodeIgniter 3.
    
    Resolves #4

--- roles/blog+catalog/templates/config.php
+++ roles/blog+catalog/templates/config.php
@@ -268,15 +268,8 @@ $config['encryption_key'] = '{{ codeigniter_encryption_key }}';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= FALSE;
-$config['sess_encrypt_cookie']	= FALSE;
-$config['sess_use_database']	= FALSE;
-$config['sess_table_name']		= 'ci_sessions';
-$config['sess_match_ip']		= FALSE;
-$config['sess_match_useragent']	= TRUE;
-$config['sess_time_to_update']	= 300;
+$config['sess_driver']          = 'memcached';
+$config['sess_save_path']       = 'localhost:11211';
 
 /*
 |--------------------------------------------------------------------------
