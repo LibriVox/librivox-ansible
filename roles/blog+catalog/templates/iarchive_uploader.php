fa2cb7b046b0a63de2f46a9c03170b96b1d740bb
redrun <redrun45@readyour.email>
2024-11-12 20:27:18 -0600

    Switch Archive API to use HTTPS

--- roles/blog+catalog/templates/iarchive_uploader.php
+++ roles/blog+catalog/templates/iarchive_uploader.php
@@ -7,7 +7,7 @@ $config['iarchive_uploader']['access_key'] = '{{ iarchive_uploader_access_key }}
 $config['iarchive_uploader']['secret_key'] = '{{ iarchive_uploader_secret_key }}';
 
 //if there is a direct iarchive alias, let's use that in case the s3 link changes
-$config['iarchive_uploader']['amazon_endpoint'] = 'http://s3.us.archive.org';
+$config['iarchive_uploader']['amazon_endpoint'] = 'https://s3.us.archive.org';
 
 // iarchive project page
 $config['iarchive_uploader']['iarchive_project_page'] = 'https://archive.org/details';
